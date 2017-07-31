324bbdbb92c833f3868db1877d5e167f52ef14de
d3rpd3rp <d3rpd3rp@users.noreply.github.com>
2017-07-31 03:08:41 -0400

    a bit cleaner, removed some print statements
    print statements help a lot in the logs

--- awsConfig/RULEdeepsecInstanceAMEvent.py
+++ awsConfig/RULEdeepsecInstanceAMEvent.py
@@ -11,7 +11,9 @@ from dsp3.models.manager import Manager
 
 import boto3
 
-#from mark n.
+#mostly taken from mark nca https://github.com/deep-security/aws-config, 
+#ported to version 3
+#added a few different cases for compliance
 def aws_config_rule_handler(event, context):
     instance_id = None
     has_policy = False
@@ -31,7 +33,7 @@ def aws_config_rule_handler(event, context):
         eventLeftScope]')
         return { 'result': 'error' }
 
-	# Convert any test events to json (only needed for direct testing through the AWS Lambda Management Console)
+    #Convert any test events to json (only needed for direct testing through the AWS Lambda Management Console)
     if 'ruleParameters' in event \
         and not type(event['ruleParameters']) == type({}): \
 		event['ruleParameters'] = json.loads(event['ruleParameters'])
@@ -40,7 +42,7 @@ def aws_config_rule_handler(event, context):
 		and not type(event['invokingEvent']) == type({}): \
 		event['invokingEvent'] = json.loads(event['invokingEvent'])
 
-	# Make sure we have the required rule parameters
+	#Make sure we have the required rule parameters
     if 'ruleParameters' in event:
         if 'deepsecUser' not in event['ruleParameters'] and \
            'deepsecPasswd' not in event['ruleParameters'] and \
@@ -80,82 +82,81 @@ def aws_config_rule_handler(event, context):
         deepsec_hostname = event['ruleParameters']['deepsecHostname'] \
         if 'deepsecHostname' in event['ruleParameters'] else None
         deepsec_manager = None
-        print('Passing {}, {}, {}, and {} to deepsecmgr.'.format(event['ruleParameters']['deepsecUser'], \
-        event['ruleParameters']['deepsecPasswd'], deepsec_tenant, deepsec_hostname))
+        success_msg = 'Successfully authenticated to Deep Security'
         try:
             if deepsec_hostname and deepsec_tenant:
                 deepsec_manager = Manager(username = event['ruleParameters']['deepsecUser'],\
                 password=event['ruleParameters']['deepsecPasswd'], \
                 tenant = deepsec_tenant, hostname = deepsec_hostname)
-                print("Successfully authenticated to Deep Security")
+                print(success_msg)
             elif deepsec_hostname and not deepsec_tenant:
                 deepsec_manager = Manager(username = event['ruleParameters']['deepsecUser'],\
                 password=event['ruleParameters']['deepsecPasswd'], hostname = deepsec_hostname)
-                print("Successfully authenticated to Deep Security")
+                print(success_msg)
             elif deepsec_tenant and not deepsec_hostname:
                 deepsec_manager = Manager(username = event['ruleParameters']['deepsecUser'],\
                 password=event['ruleParameters']['deepsecPasswd'], tenant = deepsec_tenant)
-                print("Successfully authenticated to Deep Security")
+                print(success_msg)
             else:
-                print("Missing parameters to authenticate to Deep Security Web Service.")
-        except Exception:
+                print('Missing parameters to authenticate to Deep Security Web Service.')
+        except:
             print('Authentication Error to Deep Security Manger.')
 
     if deepsec_manager and instance_id:
         stages.append(('deepsec_manager authenticate and instance id is object from aws.', 2))
         print(stages)
         malware_event = None
-		#now we need to find the ID to feed to DSP3,
+		#now we need to find the ID to feed to DSP3
         hosts = deepsec_manager.host_retrieve_all()
-        print('retrieved hosts from host_retrieve_all')
-        print('hosts: {}'.format(hosts))
         for host in hosts:
-            print('current host ID{}'.format(host['ID']))
             aws_id = deepsec_manager.host_detail_retrieve(host_id=host['ID'])\
-                    ['cloudObjectInstanceId']
-            print('retrieved aws_id from host_detail_retrieve')        
+                    ['cloudObjectInstanceId']       
             if instance_id.lower().strip() == aws_id:
                 stages.append(('found aws id in deepsec', 3))
-                print(stages)
-                print('the deepsec_manager_instance_id is: {}'.format(host['ID']))
                 cur_host_status = str(deepsec_manager.host_status(host['ID']))
                 if not 'Anti-Malware: Not Activated' in cur_host_status:
                     try:
                         malware_event = deepsec_manager.antimalware_event_retreive(time_type = "LAST_HOUR", host_id = host['ID'])
-                        print('mal event:\n {}.'.format(malware_event))
                         malware_event_str = str(malware_event)
-                        print('***')
-                        print(malware_event_str.find('antiMalwareEvents = None'))
-                        print(malware_event_str.find('antiMalwareEventID'))
-                        print('***')
                         if malware_event_str.find('antiMalwareEventID') != -1:
                             detailed_msg = 'There exists a malware event on this system.'
                             print(detailed_msg)
                             break
                     except:
-                        print('Could not retrive malware event from Deep Security.')
+                        print('Exception, error checking malware event from Deep Security.')
                 else:
                     detailed_msg = 'Anti-Malware Control Not Activated.'
 
 
     client = boto3.client('config')
 
-    print('length of stages is {}'.format(len(stages)))
-
+    #case len(stages) == 0 possibly DNE as awsconfig would not trigger the rule without an instance,
+    #or maybe it is possible with the acquisition of the aws id, but then how to report this?
     if (len(stages) == 1):
+        #reaching stage 1, array position 0, an aws instance id has been found in aws
+        #but no interaction with Deep Security API yet.
+        #Return 'General issue.' for lack of more cases.
         compliance = 'NON_COMPLIANT'
-        detailed_msg = 'General issue with Lambda query.'
+        detailed_msg = 'General issue.'
     elif (len(stages) == 2):
+        #Deep Security was authenticated but no useful data returned or other problem, 
+        #check cloudwatch logs for prints. 
         compliance = 'NON_COMPLIANT'
         detailed_msg = 'Instance found in AWS inventory, unable to confirm status in Deep Security.'
-    #found in ds inventory but no agent
     elif (len(stages) == 3):
+        #A few nested cases that could be more deliberately handled
+        #Broadly, the idea was to matches the aws id with the deep security aws id; 
+        #then there are a few different messages in the routine depending on returns.
+        #The default here is when the system is "clean" and contains no anti-malware events.
+        #This is the only way to be considered compliant.
         if not detailed_msg:
             compliance = 'COMPLIANT'
             detailed_msg = 'Instance found in Deep Security, but there exist no anti-malware event alerts.'
         else:
+            #Various messages are stored previously.
             compliance = 'NON_COMPLIANT'
     else:
+        #Catchall for something else...
         compliance = 'NON_COMPLIANT'
         detailed_msg = 'Instance status unknown.'
 
@@ -168,8 +169,6 @@ def aws_config_rule_handler(event, context):
     try:
         print("Sending results back to AWS Config")
         print('resourceId: {} is {}'.format(event['invokingEvent']['configurationItem']['resourceId'], compliance))
-        #adding stages to debug
-        print(stages)
         evaluation = {
 			'ComplianceResourceType': event['invokingEvent']['configurationItem']['resourceType'],
 			'ComplianceResourceId': event['invokingEvent']['configurationItem']['resourceId'],
@@ -199,7 +198,7 @@ def aws_config_rule_handler(event, context):
         result['response'] = response
 
     except Exception:
-        print("Exception thrown.")
+        print("Exception thrown during the send back to AWS Config.")
         result['result'] = 'failure'
 
     print(result)
