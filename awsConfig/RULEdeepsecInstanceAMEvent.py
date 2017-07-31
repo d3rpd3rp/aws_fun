8333531b9b600b56c2f3d09cba209d7f6bab3864
d3rpd3rp <d3rpd3rp@users.noreply.github.com>
2017-07-31 02:13:01 -0400

    needs more cleaning up and optimization
    soft of works, limited testing

--- awsConfig/RULEdeepsecInstanceAMEvent.py
+++ awsConfig/RULEdeepsecInstanceAMEvent.py
@@ -16,6 +16,9 @@ def aws_config_rule_handler(event, context):
     instance_id = None
     has_policy = False
     detailed_msg = ""
+    deepsec_manager = None
+    stages = []
+    malware_event_str = None
     keys = [ 'invokingEvent', 'ruleParameters', 'resultToken', 'eventLeftScope',\
     'resultToken']
     flag = False
@@ -69,6 +72,8 @@ def aws_config_rule_handler(event, context):
                         print('Event is not of resourceType of AWS::EC2::Instance')
 
     if instance_id:
+        #log that an instance id was found in aws event
+        stages = [('instance id', 1)]
 		# We know this instance ID was somehow impacted, check it's status in Deep Security
         deepsec_tenant = event['ruleParameters']['deepsecTenant'] \
         if 'deepsecTenant' in event['ruleParameters'] else None
@@ -97,52 +102,74 @@ def aws_config_rule_handler(event, context):
             print('Authentication Error to Deep Security Manger.')
 
     if deepsec_manager and instance_id:
+        stages.append(('deepsec_manager authenticate and instance id is object from aws.', 2))
+        print(stages)
         malware_event = None
 		#now we need to find the ID to feed to DSP3,
         hosts = deepsec_manager.host_retrieve_all()
+        print('retrieved hosts from host_retrieve_all')
+        print('hosts: {}'.format(hosts))
         for host in hosts:
-            try:
-                aws_id = deepsec_manager.host_detail_retrieve(host_id=host['ID'])\
-                         ['cloudObjectInstanceId']
-                if instance_id.lower().strip() == aws_id:
-                    deepsec_manager_instance_id = host['ID']
-                    print('the deepsec_manager_instance_id is: {}'.format(host['ID']))
-                    malware_event = deepsec_manager.antimalware_event_retreive(time_type = "LAST_HOUR", host_id = deepsec_manager_instance_id)
-                    #print('type of mal event {}.'.format(malware_event))
-                    #logDate = 2017-07-29 00:21:14+00:00
-                    #antiMalwareEvents = None 
-                    #print('check for None malware is {}'.format(str(malware_event).find('antiMalwareEvents = None')))
-                    #print('check for malware ID is {}'.format(str(malware_event).find('antiMalwareEventID')))
-                    #print('type for find is {}'.format(type(str(malware_event).find('antiMalwareEvents = None'))))
-                    malware_event_str = str(malware_event)
-                    print('***')
-                    print(malware_event_str.find('antiMalwareEvents = None'))
-                    print(malware_event_str.find('antiMalwareEventID'))
-                    print('***')
-                    if (malware_event_str.find('antiMalwareEventID') != '-1'):
-                        detailed_msg = 'There exists a malware event on this system.'
-                        print(detailed_msg)
-            except Exception:
-                print('derp, system not found or other error.')
-    
-    deepsec_manager.end_session() # gracefully clean up our Deep Security session
+            print('current host ID{}'.format(host['ID']))
+            aws_id = deepsec_manager.host_detail_retrieve(host_id=host['ID'])\
+                    ['cloudObjectInstanceId']
+            print('retrieved aws_id from host_detail_retrieve')        
+            if instance_id.lower().strip() == aws_id:
+                stages.append(('found aws id in deepsec', 3))
+                print(stages)
+                print('the deepsec_manager_instance_id is: {}'.format(host['ID']))
+                cur_host_status = str(deepsec_manager.host_status(host['ID']))
+                if not 'Anti-Malware: Not Activated' in cur_host_status:
+                    try:
+                        malware_event = deepsec_manager.antimalware_event_retreive(time_type = "LAST_HOUR", host_id = host['ID'])
+                        print('mal event:\n {}.'.format(malware_event))
+                        malware_event_str = str(malware_event)
+                        print('***')
+                        print(malware_event_str.find('antiMalwareEvents = None'))
+                        print(malware_event_str.find('antiMalwareEventID'))
+                        print('***')
+                        if malware_event_str.find('antiMalwareEventID') != -1:
+                            detailed_msg = 'There exists a malware event on this system.'
+                            print(detailed_msg)
+                            break
+                    except:
+                        print('Could not retrive malware event from Deep Security.')
+                else:
+                    detailed_msg = 'Anti-Malware Control Not Activated.'
 
-	# Report the results back to AWS Config
+
+    client = boto3.client('config')
+
+    print('length of stages is {}'.format(len(stages)))
+
+    if (len(stages) == 1):
+        compliance = 'NON_COMPLIANT'
+        detailed_msg = 'General issue with Lambda query.'
+    elif (len(stages) == 2):
+        compliance = 'NON_COMPLIANT'
+        detailed_msg = 'Instance found in AWS inventory, unable to confirm status in Deep Security.'
+    #found in ds inventory but no agent
+    elif (len(stages) == 3):
+        if not detailed_msg:
+            compliance = 'COMPLIANT'
+            detailed_msg = 'Instance found in Deep Security, but there exist no anti-malware event alerts.'
+        else:
+            compliance = 'NON_COMPLIANT'
+    else:
+        compliance = 'NON_COMPLIANT'
+        detailed_msg = 'Instance status unknown.'
+
+    # Report the results back to AWS Config
     if detailed_msg:
         result = { 'annotation': detailed_msg }
     else:
         result = {}
 
-    client = boto3.client('config')
-    if (malware_event_str.find('antiMalwareEvents = None') == '-1'):
-        compliance = 'COMPLIANT'
-    elif (malware_event_str.find('antiMalwareEventID') != '-1'):
-        compliance = 'NON_COMPLIANT'
-		#27 July 2017, not sure why this is indented or needs to be...
     try:
         print("Sending results back to AWS Config")
         print('resourceId: {} is {}'.format(event['invokingEvent']['configurationItem']['resourceId'], compliance))
-
+        #adding stages to debug
+        print(stages)
         evaluation = {
 			'ComplianceResourceType': event['invokingEvent']['configurationItem']['resourceType'],
 			'ComplianceResourceId': event['invokingEvent']['configurationItem']['resourceId'],
@@ -150,7 +177,7 @@ def aws_config_rule_handler(event, context):
 			'OrderingTimestamp': datetime.datetime.now() }
 			
         print('after evaluation assignment 1.')
-        
+
         if detailed_msg:
             evaluation['Annotation'] = detailed_msg
             print('after evaluation assignment 2.')
